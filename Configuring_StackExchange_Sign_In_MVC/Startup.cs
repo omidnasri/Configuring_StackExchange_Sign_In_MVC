64212cfdc910e25f08d553e1298f2b091ec1cdc6
Omid Nasri <omid.nasri@outlook.com>
2016-02-05 14:07:06 +0330

    First Commit.

---
+++ Configuring_StackExchange_Sign_In_MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Configuring_StackExchange_Sign_In_MVC.Startup))]
+namespace Configuring_StackExchange_Sign_In_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
